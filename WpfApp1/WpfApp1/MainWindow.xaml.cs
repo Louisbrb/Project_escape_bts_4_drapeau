0a485b92e9cfe531ce465b8c794f58c1fd0810b2
Odil <43874637+Odiltan@users.noreply.github.com>
2018-11-13 12:09:13 +0100

    13/11/2018
    Réalisé :
    -	Champ texte fini avec correcteur d’orthographe.
    -	Bouton valider fini avec vérification de la réponse et un pop-up qui affiche le résultat.
    -	Bouton Indice fini avec les indices qui s’affichent sur un pop-up.

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,60 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void valider_Click(object sender, RoutedEventArgs e)
+        {
+            if (champReponse.Text[champReponse.Text.Length - 1] == ' ')
+            {
+                champReponse.SelectionStart = 0;
+                champReponse.SelectionLength = champReponse.Text.Length - 1;
+                champReponse.Text = champReponse.SelectedText;
+            }
+            if (champReponse.Text == "réponse")
+            {
+                MessageBox.Show("Bravo! Vous avez résolu l'enigme.");
+            }
+            else if (champReponse.Text != "réponse")
+            {
+                MessageBox.Show("FAUX! Réessayez!");
+                champReponse.Text = "";
+            }
+        }
+
+        private void Indices_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Insérez indice ici!\nInsérez indice ici\nInsérer indice ici");
+        }
+
+        private void mehdi_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Il y a un correcteur d'ortographe.\nIl suffit d'appuyer sur \"ESPACE\" à la fin de votre réponse.\n\n-Mehdi （ゝ‿ ・）");
+        }
+
+        private void champReponse_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (champReponse.Text == "Entrez réponse")
+            {
+                champReponse.Text = "";
+            }
+            else if (champReponse.Text != "Entrez réponse")
+            {
+                champReponse.SelectionStart = 0;
+                champReponse.SelectionLength = champReponse.Text.Length;
+            }
+        }
+
+        private void champReponse_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (champReponse.Text == "Entrez réponse")
+            {
+                champReponse.Text = "";
+            }
+            else if (champReponse.Text != "Entrez réponse")
+            {
+                champReponse.SelectionStart = 0;
+                champReponse.SelectionLength = champReponse.Text.Length;
+            }
+        }
     }
 }
