106b5af7c2d6424f7a42a77ec398b0fc32ebf11e
Odil <43874637+Odiltan@users.noreply.github.com>
2018-11-23 11:05:21 +0100

    23/11/2018

--- Interface Réponse 'Alfred'/WpfApp1/MainWindow.xaml.cs
+++ Interface Réponse 'Alfred'/WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
 namespace WpfApp1
 {
