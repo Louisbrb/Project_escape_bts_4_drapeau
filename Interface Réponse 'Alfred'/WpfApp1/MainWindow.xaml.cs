59cb7b5e6900e9d0a0491d550367491a94afce10
Odil <43874637+Odiltan@users.noreply.github.com>
2018-11-27 12:00:27 +0100

    27/11/2018

--- Interface Réponse 'Alfred'/WpfApp1/MainWindow.xaml.cs
+++ Interface Réponse 'Alfred'/WpfApp1/MainWindow.xaml.cs
@@ -23,12 +23,8 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        private TcpClient client;
-        private TcpListener listener;
         private String IP = "10.16.3.214";
         private int port = 53000;
-        private Socket socket;
 
         public MainWindow()
         {
@@ -45,12 +41,12 @@ namespace WpfApp1
             }
             if (champReponse.Text == "réponse")
             {
-                client = new TcpClient(IP, port);
-                NetworkStream nwStream = client.GetStream();
-                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes("GAGNE:04");
+                //client = new TcpClient(IP, port);
+                //NetworkStream nwStream = client.GetStream();
+                //byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes("GAGNE:04");
 
                 //Console.WriteLine("Sending : GAGNE:04");
-                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                // nwStream.Write(bytesToSend, 0, bytesToSend.Length);
                 // try
                 //{
                 //   client.Connect(IP,port);
@@ -59,24 +55,35 @@ namespace WpfApp1
                 //{
                 //     MessageBox.Show("Error : " + ex.Message);
                 // }
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ipAdd = System.Net.IPAddress.Parse(IP);
+                IPEndPoint remoteEP = new IPEndPoint(ipAdd, port);
 
-                MessageBox.Show("Bravo! Vous avez résolu l'enigme.");
+                socket.Connect(remoteEP);
+
+                byte[] byData = System.Text.Encoding.ASCII.GetBytes("GAGNE:4");
+                socket.Send(byData);
+
+
+                socket.Disconnect(false);
+                socket.Close();
+                MessageBox.Show("Bravo! Vous avez résolu l'enigme.", "Bravo!");
             }
-            else if (champReponse.Text != "réponse")
+            else if (champReponse.Text != "Laos")
             {
-                MessageBox.Show("FAUX! Réessayez!");
+                MessageBox.Show("FAUX! Réessayez!", "Raté");
                 champReponse.Text = "";
             }
         }
 
         private void Indices_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Insérez indice ici!\nInsérez indice ici\nInsérer indice ici");
+            MessageBox.Show("Insérez indice ici!\nInsérez indice ici\nInsérer indice ici", "Indices");
         }
 
         private void mehdi_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Il y a un correcteur d'ortographe.\nIl suffit d'appuyer sur \"ESPACE\" à la fin de votre réponse.\n\n-Mehdi （ゝ‿ ・）");
+            MessageBox.Show("Il y a un correcteur d'ortographe.\nIl suffit d'appuyer sur \"ESPACE\" à la fin de votre réponse.\n\n-Mehdi （ゝ‿ ・）","Note du dev");
         }
 
         private void champReponse_GotFocus(object sender, RoutedEventArgs e)
