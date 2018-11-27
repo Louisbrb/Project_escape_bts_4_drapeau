d4bc44d400e49744d5548e9fefa137dd60e7b739
Odil <43874637+Odiltan@users.noreply.github.com>
2018-11-27 12:12:08 +0100

    27/11/2018

--- Interface Réponse 'Alfred'/WpfApp1/MainWindow.xaml.cs
+++ Interface Réponse 'Alfred'/WpfApp1/MainWindow.xaml.cs
@@ -23,14 +23,16 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private String IP = "10.16.3.214";
-        private int port = 53000;
+        private String reponse = "Laos"; //la bonne réponse
+        private String IP = "10.16.3.214"; //l'adresse IP du serveur
+        private int port = 53000; //le port d'écoute du serveur
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //bouton valider
         private void valider_Click(object sender, RoutedEventArgs e)
         {
             if (champReponse.Text[champReponse.Text.Length - 1] == ' ')
@@ -39,22 +41,11 @@ namespace WpfApp1
                 champReponse.SelectionLength = champReponse.Text.Length - 1;
                 champReponse.Text = champReponse.SelectedText;
             }
-            if (champReponse.Text == "réponse")
-            {
-                //client = new TcpClient(IP, port);
-                //NetworkStream nwStream = client.GetStream();
-                //byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes("GAGNE:04");
 
-                //Console.WriteLine("Sending : GAGNE:04");
-                // nwStream.Write(bytesToSend, 0, bytesToSend.Length);
-                // try
-                //{
-                //   client.Connect(IP,port);
-                //}
-                // catch (Exception ex)
-                //{
-                //     MessageBox.Show("Error : " + ex.Message);
-                // }
+            //condition pour une bonne réponse
+            if (champReponse.Text == reponse)
+            {
+                //envoie d'un string au serveur via socket
                 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 IPAddress ipAdd = System.Net.IPAddress.Parse(IP);
                 IPEndPoint remoteEP = new IPEndPoint(ipAdd, port);
@@ -64,28 +55,33 @@ namespace WpfApp1
                 byte[] byData = System.Text.Encoding.ASCII.GetBytes("GAGNE:4");
                 socket.Send(byData);
 
-
+                //deconnexion du socket pour le réutiliser et message de victoire
                 socket.Disconnect(false);
                 socket.Close();
-                MessageBox.Show("Bravo! Vous avez résolu l'enigme.", "Bravo!");
+                MessageBox.Show("Bravo! Vous avez résolu l'enigme.", "Victoire!");
             }
-            else if (champReponse.Text != "Laos")
+
+            //mauvaise réponse
+            else if (champReponse.Text != reponse)
             {
                 MessageBox.Show("FAUX! Réessayez!", "Raté");
                 champReponse.Text = "";
             }
         }
 
+        //bouton indice
         private void Indices_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Insérez indice ici!\nInsérez indice ici\nInsérer indice ici", "Indices");
         }
 
+        //bouton message du dev
         private void mehdi_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Il y a un correcteur d'ortographe.\nIl suffit d'appuyer sur \"ESPACE\" à la fin de votre réponse.\n\n-Mehdi （ゝ‿ ・）","Note du dev");
         }
 
+        //selection du texte lors d'un focus sur le champ de texte
         private void champReponse_GotFocus(object sender, RoutedEventArgs e)
         {
             if (champReponse.Text == "Entrez réponse")
@@ -99,6 +95,7 @@ namespace WpfApp1
             }
         }
 
+        //selection du texte lors d'un clic sur le champ de texte
         private void champReponse_GotMouseCapture(object sender, MouseEventArgs e)
         {
             if (champReponse.Text == "Entrez réponse")
